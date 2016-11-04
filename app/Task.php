b2a1f5239fcd3fc8d5a69cc2dbd7b57bb0613941
David Martinez <davidmgilo@gmail.com>
2016-11-04 16:38:43 +0100

    relacions 2

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable= ['name', 'done', 'priority'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
