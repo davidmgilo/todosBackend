87dd2259e86433a0589bb96b2ece4515e2dca8c2
David Martinez <davidmgilo@gmail.com>
2016-11-04 16:01:35 +0100

    task transform && exception

---
+++ app/Transformers/TaskTransformer.php
@@ -0,0 +1,19 @@
+<?php
+use App\Exceptions\IncorrectModelException;
+
+
+class TaskTransformer
+{
+    protected function transform($resource)
+    {
+        if(! $resource instanceof \App\Task){
+            throw new IncorrectModelException();
+        }
+        return [
+            'name' => $resource['name'],
+            'done' => (boolean)$resource['done'],
+            'priority' => (integer)$resource['priority'],
+
+        ];
+    }
+}
