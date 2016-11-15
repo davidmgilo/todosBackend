0d701448ca0c70462d98b78e709aedcc416bbfc2
David Martinez <davidmgilo@gmail.com>
2016-11-15 20:39:28 +0100

    preguntes fetes

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::group(['prefix' => 'v1'], function () {
     Route::resource('task', 'TasksController');
     Route::resource('user', 'UsersController');
     Route::resource('user.task', 'UserTasksController');
-//    Route::resource('task.user','TaskUserController'); TODO
+//    Route::resource('task.user','TaskUserController');
 });
