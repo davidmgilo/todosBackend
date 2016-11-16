14e0fc1a5f5dc012d6c85236dd9730500809d81b
David Martinez <davidmgilo@gmail.com>
2016-11-16 20:03:18 +0100

    vue.js ajax 1

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -41,6 +41,18 @@ const app = new Vue({
     methods: {
         reverseMessage: function(){
             this.message = this.message.split('').reverse().join('');
+        },
+        fetchData: function (){
+            //Axios
+            var req = new XMLHttpRequest();
+            req.open('GET', 'http://localhost:8082/api/v1/task', false);
+            req.send(null);
+            if (req.status == 200)
+                console.log(req.responseText);
         }
+    },
+    created: function() {
+        console.log('App created');
+        this.fetchData()
     }
 });
