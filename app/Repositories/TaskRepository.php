bb95173e4d3ff438627ddd332582fe0c27992187
davidmgilo <davidmgilo@gmail.com>
2016-11-18 12:33:05 +0000

    Applied fixes from StyleCI

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -11,7 +11,7 @@ use App\Task;
 class TaskRepository implements Repository
 {
     /**
-     * @param integer $id
+     * @param int   $id
      * @param array $columns
      *
      * @return mixed
