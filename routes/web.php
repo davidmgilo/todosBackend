83c7bf0d63cd7b47ddcd813e9b3e985dba092bab
David Martinez <davidmgilo@gmail.com>
2016-12-13 19:35:35 +0100

    gate define

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,26 @@
 <?php
 
+Gate::define('impossible-gate', function () {
+    return false;
+});
+
+Gate::define('easy-gate', function () {
+    return true;
+});
+
+Gate::define('update-task', function ($user, $task) {
+    return $user->id == $task->user_id;
+});
+
+Gate::define('update-task1', function ($user) {
+    return $user->isAdmin();
+});
+
+Gate::define('update-task', function ($user, $task) {
+    if($user->isAdmin()) return true;
+    return $user->id == $task->user_id;
+});
+
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/tasks', function () {
         return view('tasks');
