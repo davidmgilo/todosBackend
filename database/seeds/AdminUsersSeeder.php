e5eab88774ac812d4c199eae2a7b438d848bc453
David Martinez Gilo <davidmgilo@gmail.com>
2016-12-14 13:21:26 +0100

    seed for admin user with admin role

---
+++ database/seeds/AdminUsersSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class AdminUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try {
+            $user= factory(App\User::class)->create([
+                    "name" => "David Martinez Gilo",
+                    "email" => "davidmgilo@gmail.com",
+                    "password" => bcrypt(env('DAVID_PWD', '123456'))]
+            );
+            Role::create(['name' => 'admin']);
+            $user->assignRole('admin');
+        } catch (\Illuminate\Database\QueryException $exception) {
+        }
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(TasksTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(AdminUsersSeeder::class);
     }
 }
