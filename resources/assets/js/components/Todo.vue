b020e087cda95563fe0496791f06961473f2ffbe
David Martinez Gilo <davidmgilo@gmail.com>
2016-12-18 22:53:31 +0100

    added script for icheck commented as it doesn't work well - see computed watchers

--- resources/assets/js/components/Todo.vue
+++ resources/assets/js/components/Todo.vue
@@ -123,6 +123,13 @@
         }
     }
 
+//    $(function () {
+//            $('input').iCheck({
+//                checkboxClass: 'icheckbox_square-green',
+//                radioClass: 'iradio_square-green',
+//                increaseArea: '20%' // optional
+//            });
+//        });
 </script>
 
 
