f3218de847dc4f9f814070e51b48f558fdffcc44
David Martinez <davidmgilo@gmail.com>
2017-01-10 18:21:14 +0100

    layout - styles - container

--- resources/views/boxmodel.blade.php
+++ resources/views/boxmodel.blade.php
@@ -20,37 +20,46 @@
 
 #section {
 	border: solid red 1px;
+	margin-left:200px;
+	padding: 10px;
 }
 
 #footer{
 	border: solid blueviolet 1px;
 }
 
+#container{
+	position:relative;
+}
+
 </style>
 
-<div id="nav">
-	<ul>
-		<li>Link 1</li>
-		<li>Link 2</li>
-		<li>Link 3</li>
-		<li>Contact</li>
-		<li>About</li>
-	</ul>
+<div id="container">
+	<div id="nav">
+		<ul>
+			<li>Link 1</li>
+			<li>Link 2</li>
+			<li>Link 3</li>
+			<li>Contact</li>
+			<li>About</li>
+		</ul>
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet eaque eum exercitationem fugit hic incidunt labore nobis sit voluptate! At culpa, cum delectus enim esse iusto natus recusandae sit unde?
+	</div>
+
+	<div id="section">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur iure maxime officia rem? Aspernatur assumenda atque consequatur deleniti, dolor excepturi magnam maxime minus mollitia officiis perferendis, sed vel voluptatibus.
+	</div>
+
+
+
+	<div id="footer">
+		@copyright adsafsa asdasd da
+	</div>
 </div>
 
-<div id="section">
- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet eaque eum exercitationem fugit hic incidunt labore nobis sit voluptate! At culpa, cum delectus enim esse iusto natus recusandae sit unde?
-</div>
-
-<div id="section">
- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur iure maxime officia rem? Aspernatur assumenda atque consequatur deleniti, dolor excepturi magnam maxime minus mollitia officiis perferendis, sed vel voluptatibus.
-</div>
-
-
-
-<div id="footer">
-	@copyright adsafsa asdasd da
-</div>
 
 </body>
 </html>
