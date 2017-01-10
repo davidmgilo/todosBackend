0ed18435af30022d57b60e4a409e9c5d69e4f871
David Martinez <davidmgilo@gmail.com>
2017-01-10 18:13:21 +0100

    created html layout

--- resources/views/boxmodel.blade.php
+++ resources/views/boxmodel.blade.php
@@ -11,8 +11,32 @@
 <body>
 
 <style>
-	
+
 </style>
+
+<div id="sidebar">
+	<ul>
+		<li>Link 1</li>
+		<li>Link 2</li>
+		<li>Link 3</li>
+		<li>Contact</li>
+		<li>About</li>
+	</ul>
+</div>
+
+<div id="section">
+ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet eaque eum exercitationem fugit hic incidunt labore nobis sit voluptate! At culpa, cum delectus enim esse iusto natus recusandae sit unde?
+</div>
+
+<div id="section">
+ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur iure maxime officia rem? Aspernatur assumenda atque consequatur deleniti, dolor excepturi magnam maxime minus mollitia officiis perferendis, sed vel voluptatibus.
+</div>
+
+
+
+<div id="footer">
+	@copyright adsafsa asdasd da
+</div>
 
 </body>
 </html>
