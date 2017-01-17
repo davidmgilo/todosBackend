bf79a3e09b136033f857be4b1f509e7c9fabbb5f
David Martinez <davidmgilo@gmail.com>
2017-01-17 18:18:49 +0100

    layouts - bootstraplayout II

--- app/Http/Controllers/BootstraplayoutController.php
+++ app/Http/Controllers/BootstraplayoutController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
 
-class BootstraplayoutController extends Controller
+class BootstraplayoutController extends BaseController
 {
     //
     /**
