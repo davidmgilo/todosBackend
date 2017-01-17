86bf8ed23f2135d23bc38c8609073ae7381af617
David Martinez <davidmgilo@gmail.com>
2017-01-17 18:23:50 +0100

    layouts - bootstraplayout III

--- resources/views/bootstraplayout.blade.php
+++ resources/views/bootstraplayout.blade.php
@@ -17,7 +17,17 @@
 </head>
 <body>
 
-<h1>Prova</h1>
+<div class="container">
+	<div class="jumbotron">
+		<h1>Prova</h1>
+	</div>
+	<div class="row">
+		<div class="col-md-4">Column 1</div>
+		<div class="col-md-4">Column 2</div>
+		<div class="col-md-4">Column 3</div>
+	</div>
+
+</div>
 
 </body>
 </html>
