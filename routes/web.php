01943c99ed67d2f7b92c0b07453fffa86560a318
David Martinez <davidmgilo@gmail.com>
2017-01-17 16:55:32 +0100

    layouts - float - make:vc flexboxlayout

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ Route::group(['middleware' => 'auth'], function () {
     });
 
     #adminlte_routes
+    Route::get('flexboxlayout', 'FlexboxlayoutController@index')->name('flexboxlayout');
+
     Route::get('csstables', 'CsstablesController@index')->name('csstables');
 
     Route::get('layoutfloat', 'LayoutfloatController@index')->name('layoutfloat');
