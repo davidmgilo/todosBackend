1d635fb89ca8994191eb30ed2e1014ca37b3c45a
David Martinez <davidmgilo@gmail.com>
2017-01-17 21:41:33 +0100

    laravel-cors

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ use Illuminate\Http\Request;
 //})->middleware('auth:api');
 // Les APIs han de ser Stateless. Cada petició ha de ser autenticada. Auth.basic no ho fa del tot.
 Route::group(['prefix' => 'v1',
-    'middleware'       => 'auth:api',
+//    'middleware'       => 'auth:api',
+    'middleware'       => 'cors',
         ],
     function () {
         Route::resource('task', 'TasksController');
