2493a943de4bc6142aea2295bfa906ec2e3d6853
David Martinez <davidmgilo@gmail.com>
2017-03-28 21:53:49 +0200

    controllers for chat

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Davidmgilo\TodosBackend\Http\Controllers;
+
+use Davidmgilo\chat\Events\MessageSent;
+use Davidmgilo\chat\Message;
+use Auth;
+use Illuminate\Http\Request;
+
+/**
+ * Class ChatController
+ * @package Davidmgilo\chat\Http\Controllers
+ */
+class ChatController extends ChatBaseController
+{
+    //
+    /**
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $data = [];
+        return view('chat',$data);
+    }
+
+    /**
+     * Persist message to database
+     *
+     * @param  Request $request
+     * @return array
+     */
+    public function sendMessage(Request $request)
+    {
+        $user = Auth::user();
+
+        $message = $user->messages()->create([
+            'message' => $request->input('message')
+        ]);
+
+        //Broadcast
+
+        broadcast(new MessageSent($user, $message))->toOthers();
+
+        return ['status' => 'Message Sent!'];
+    }
+
+    public function fetchMessages()
+    {
+        //Lazy loading -> Eager Loading
+        return Message::with('user')->get();
+    }
+
+}
