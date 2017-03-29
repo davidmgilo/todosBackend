da0c95ee989ef20029468c7c4fd29f7b397e5941
David Martinez <davidmgilo@gmail.com>
2017-03-29 19:45:26 +0200

    https

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -63,6 +63,7 @@ window.io = io
 
 window.Echo = new Echo({
   broadcaster: 'socket.io',
-  host: window.location.hostname + ':6001',
+  // host: window.location.hostname + ':6001',
+  host: window.Laravel.echoServerURL,
   namespace: 'Davidmgilo.chat.Events'
 })
