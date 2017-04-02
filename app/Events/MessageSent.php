fbd35ddfb7e9e5111d281c221d7ea209cfd19ace
David Martínez Gilo <davidmgilo@gmail.com>
2017-04-02 19:36:36 +0200

    not needed anymore

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -8,7 +8,7 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\PresenceChannel;
-use Davidmgilo\TodosBackend\Events\Dispatchable;
+use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
